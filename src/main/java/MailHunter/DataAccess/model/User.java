09cec0a8ccd21045f7e497ba47e5f72cb299d840
Vladislav Sidlyarevich <vlsidlyarevich@gmail.com>
2016-04-17 12:46:02 +0300

    user model added

--- src/main/java/MailHunter/DataAccess/model/User.java
+++ src/main/java/MailHunter/DataAccess/model/User.java
@@ -1,4 +1,86 @@
 package MailHunter.DataAccess.model;
 
-public class User {
+
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.domain.Persistable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User implements Persistable<Integer> {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Column(name="id",unique = true,nullable = false)
+    private Integer id;
+
+    @Column(name = "username",length = 45,nullable = false)
+    private String username;
+    @Column(name ="password", length = 45,nullable = false)
+    private String password;
+    @Column(name="role",length = 45,nullable = false)
+    private String role;
+
+    public User(){
+        super();
+    }
+
+    public User(Integer id, String username, String password, String role) {
+        super();
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    @Transient
+    public boolean isNew() {
+        return id == null;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
 }
